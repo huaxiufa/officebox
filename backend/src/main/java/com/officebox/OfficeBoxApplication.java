89b1e0bbca44ddd60d3f00fa0654a9d7db18e06d
huaxiufa <huaxiufa@gmail.com>
2026-08-27 16:17:42 +0800

    feat: add OfficeBox Spring Boot application

---
+++ backend/src/main/java/com/officebox/OfficeBoxApplication.java
@@ -0,0 +1,9 @@
+package com.officebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeBoxApplication {
+  public static void main(String[] args) { SpringApplication.run(OfficeBoxApplication.class, args); }
+}
